065cbff70a6b3d5b31d7ff4980e69e2ee7cf6b40
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-04-07 19:52:16 +0200

    Logo updated for dark/light-mode

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,12 @@ import DarkModeButton from "../DarkModeButton/DarkModeButton";
 const Navbar = ({ isDark, setIsDark }) => {
     return <Box className='Navbar'>
         <h1 className="TheFlagApp">The Flag App</h1>
-        <img src="src\assets\techover-logo.png" alt="" />
+
+        {isDark ? (<img src="src\assets\techover-logo.png" alt="" />)
+        :
+        (<img src="src\assets\techover-logo-dark.png" alt="" />)}
+
+
         <DarkModeButton isDark={isDark} setIsDark={setIsDark} />
     </Box>
 };
