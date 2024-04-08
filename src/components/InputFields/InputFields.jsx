3254443ab2cc064504fe5597f2d157e6db001ca0
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-04-08 10:51:06 +0200

    Styling of the country TextField

--- src/components/InputFields/InputFields.jsx
+++ src/components/InputFields/InputFields.jsx
@@ -1,39 +1,87 @@
-import { Box, Select, TextField } from "@mui/material";
 import "./InputFields.css";
+import { Box, Select, TextField } from "@mui/material";
+import { styled } from '@mui/system';
+
+const CustomTextField = styled(TextField)(({ theme }) => ({
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#bdbdbd', // Gray border color
+    },
+    '&:hover fieldset': {
+      borderColor: '#fff', // Border color on hover
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#fff', // Border color when focused
+    },
+  },
+  '& .MuiInputLabel-root': {
+    color: '#bdbdbd', // Label color
+    '&.Mui-focused': {
+      color: '#fff', // Label color when focused
+    },
+  },
+  '& .MuiInputBase-input': {
+    color: '#bdbdbd', // Text color
+    '&:focus': {
+      color: '#fff', // Text color when focused
+    },
+  },
+}));
+
+const CustomSelect = styled(Select)(({ theme }) => ({
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#bdbdbd', // Gray border color
+    },
+    '&:hover fieldset': {
+      borderColor: '#fff', // Border color on hover
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#fff', // Border color when focused
+    },
+  },
+  '& .MuiInputLabel-root': {
+    color: '#bdbdbd', // Label color
+    '&.Mui-focused': {
+      color: '#fff', // Label color when focused
+    },
+  },
+  '& .MuiSelect-icon': {
+    color: '#fff', // Arrow color
+  },
+  '& .MuiSelect-select.MuiSelect-select': {
+    color: '#bdbdbd', // Text color
+    '&:focus': {
+      color: '#fff', // Text color when focused
+    },
+  },
+}));
 
 const InputFields = () => {
   return (
     <Box className="InputFields">
-      <TextField
+      <CustomTextField
         className="InputCountry"
         id="input-country"
         label="Search for a country"
         fontWeight="300"
         variant="outlined"
         sx={{
-          // Root class for the input field
-          "& .MuiOutlinedInput-root": {
-            color: "#000",
-            fontFamily: "Arial",
-            fontWeight: "bold",
-            // Class for the border around the input field
-            "& .MuiOutlinedInput-notchedOutline": {
-              borderColor: "#2e2e2e",
-              borderWidth: "2px",
-            },
-          },
-          // Class for the label of the input field
-          "& .MuiInputLabel-outlined": {
-            color: "#2e2e2e",
-            fontWeight: "bold",
+          '& .MuiOutlinedInput-root': {
+            borderColor: '#bdbdbd', // Gray border color
           },
         }}
       />
-      <Select
+      <CustomSelect
         className="SelectRegion"
         id="select-region"
         fontWeight="300"
         variant="outlined"
+        sx={{
+          '& .MuiOutlinedInput-root': {
+            borderColor: '#bdbdbd', // Gray border color
+          },
+        }}
       />
     </Box>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,21 @@
 import { Box } from "@mui/material";
-import './Navbar.css'
+import "./Navbar.css";
 import DarkModeButton from "../DarkModeButton/DarkModeButton";
 
-
 const Navbar = ({ isDark, setIsDark }) => {
-    return <Box className='Navbar'>
-        <h1 className="TheFlagApp">The Flag App</h1>
+  return (
+    <Box className="Navbar">
+      <h1 className="TheFlagApp">The Flag App</h1>
 
-        {isDark ? (<img src="src\assets\techover-logo.png" alt="" />)
-        :
-        (<img src="src\assets\techover-logo-dark.png" alt="" />)}
+      {isDark ? (
+        <img src="src\assets\techover-logo.png" alt="" />
+      ) : (
+        <img src="src\assets\techover-logo-dark.png" alt="" />
+      )}
 
-
-        <DarkModeButton isDark={isDark} setIsDark={setIsDark} />
+      <DarkModeButton isDark={isDark} setIsDark={setIsDark} />
     </Box>
+  );
 };
 
 export default Navbar;
