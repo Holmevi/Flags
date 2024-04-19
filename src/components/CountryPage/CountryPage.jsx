898b92c42b201568eac4ac4f30f6db421c141c10
Jimmy Holmevi <jimmy_holmevi@hotmail.com>
2024-04-19 08:29:36 +0200

    Design of country page

--- src/components/CountryPage/CountryPage.jsx
+++ src/components/CountryPage/CountryPage.jsx
@@ -15,42 +15,51 @@ const CountryPage = ({
 }) => {
   return (
     <Box className="CountryPage">
-      <img className="Flag" src={flag} alt={`${country} flag`} />
-      <Box className="InfoField">
-        <p className="CountryName">{country}</p>
-        <Box className="InfoPair">
-          <p className="InfoLabel">Population:</p>
-          <p className="Info">{population}</p>
-        </Box>
-        <Box className="InfoPair">
-          <p className="InfoLabel">Region:</p>
-          <p className="Info">{region}</p>
-        </Box>
-        <Box className="InfoPair">
-          <p className="InfoLabel">Capital:</p>
-          <p className="Info">{capital}</p>
-        </Box>
-        <Box className="InfoPair">
-          <p className="InfoLabel">Native Name:</p>
-          <p className="Info">{nativeName}</p>
-        </Box>
-        <Box className="InfoPair">
-          <p className="InfoLabel">Top Level Domain:</p>
-          <p className="Info">{topLevelDomain}</p>
-        </Box>
-        <Box className="InfoPair">
-          <p className="InfoLabel">Currencies:</p>
-          <p className="Info">{currencies}</p>
-        </Box>
-        <Box className="InfoPair">
-          <p className="InfoLabel">Language:</p>
-          <p className="Info">{language}</p>
-        </Box>
-        <Box className="InfoPair">
-          <p className="InfoLabel">Borders:</p>
-          <p className="Info">
-            {borders.length > 0 ? borders.join(", ") : "N/A"}
-          </p>
+      <Box className="FlagFieldCP">
+        <img className="FlagCP" src={flag} alt={`${country} flag`} />
+      </Box>
+
+      <Box className="InfoFieldCP">
+        <p className="CountryNameCP">{country}</p>
+        <Box className="InfoColumnPairCP">
+
+          <Box className="InfoColumnCP">
+            
+            <Box className="InfoPairCP">
+              <p className="InfoLabelCP">Population:</p>
+              <p className="InfoCP">{population}</p>
+            </Box>
+            <Box className="InfoPairCP">
+              <p className="InfoLabelCP">Region:</p>
+              <p className="InfoCP">{region}</p>
+            </Box>
+            <Box className="InfoPairCP">
+              <p className="InfoLabelCP">Capital:</p>
+              <p className="InfoCP">{capital}</p>
+            </Box>
+            <Box className="InfoPairCP">
+              <p className="InfoLabelCP">Native Name:</p>
+              <p className="InfoCP">{nativeName}</p>
+            </Box>
+            <Box className="InfoPairCP">
+              <p className="InfoLabelCP">Top Level Domain:</p>
+              <p className="InfoCP">{topLevelDomain}</p>
+            </Box>
+            <Box className="InfoPairCP">
+              <p className="InfoLabelCP">Currencies:</p>
+              <p className="InfoCP">{currencies}</p>
+            </Box>
+            <Box className="InfoPairCP">
+              <p className="InfoLabelCP">Language:</p>
+              <p className="InfoCP">{language}</p>
+            </Box>
+            <Box className="InfoPairCP">
+              <p className="InfoLabelCP">Borders:</p>
+              <p className="InfoCP">
+                {borders.length > 0 ? borders.join(", ") : "N/A"}
+              </p>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </Box>
